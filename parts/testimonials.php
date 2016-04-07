231b15d773a6b750bc80dbf57c4ef36582a3f3c4
Jacob Dubail <jacob@jacobdubail.com>
2016-04-07 14:08:10 -0700

    testimonial quotes

--- parts/testimonials.php
+++ parts/testimonials.php
@@ -69,17 +69,17 @@
 				</span>
 				<blockquote>
 					<p>
-					<span class="testimonials__quote testimonials__quote--left">
-						<svg class="icon icon-left-quote">
-							<use xlink:href="#icon-left-quote"></use>
-						</svg>
-					</span>
-					Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.
-					<span class="testimonials__quote testimonials__quote--right">
-						<svg class="icon icon-right-quote">
-							<use xlink:href="#icon-right-quote"></use>
-						</svg>
-					</span>
+						<span class="testimonials__quote testimonials__quote--left">
+							<svg class="icon icon-left-quote">
+								<use xlink:href="#icon-left-quote"></use>
+							</svg>
+						</span>
+						Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.
+						<span class="testimonials__quote testimonials__quote--right">
+							<svg class="icon icon-right-quote">
+								<use xlink:href="#icon-right-quote"></use>
+							</svg>
+						</span>
 					</p>
 
 					<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
@@ -106,7 +106,20 @@
 					</svg>
 				</span>
 				<blockquote>
-					<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
+					<p>
+						<span class="testimonials__quote testimonials__quote--left">
+							<svg class="icon icon-left-quote">
+								<use xlink:href="#icon-left-quote"></use>
+							</svg>
+						</span>
+						Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.
+						<span class="testimonials__quote testimonials__quote--right">
+							<svg class="icon icon-right-quote">
+								<use xlink:href="#icon-right-quote"></use>
+							</svg>
+						</span>
+					</p>
+
 					<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
 				</blockquote>
 				<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
