38b17a3557e7d1c5423643f3a956db8e99bdb53c
Jacob Dubail <jacob@jacobdubail.com>
2016-04-07 13:59:55 -0700

    adding testimonials bg

--- parts/testimonials.php
+++ parts/testimonials.php
@@ -5,82 +5,87 @@
 ?>
 
 <section class="testimonials">
+	<div class="container">
 
-	<div class="owl-carousel">
+		<div class="owl-carousel">
 
-		<div class="item">
-			<span class="welcome__stars">
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-			</span>
-			<blockquote>
-				<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
-				<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
-			</blockquote>
-			<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
+			<div class="item">
+				<span class="welcome__stars">
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+				</span>
+				<blockquote>
+					<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
+					<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
+				</blockquote>
+				<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
+			</div>
+
+			<div class="item">
+				<span class="welcome__stars">
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+				</span>
+				<blockquote>
+					<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
+					<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
+				</blockquote>
+				<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
+			</div>
+
+			<div class="item">
+				<span class="welcome__stars">
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+					<svg class="icon icon-star">
+						<use xlink:href="#icon-star"></use>
+					</svg>
+				</span>
+				<blockquote>
+					<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
+					<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
+				</blockquote>
+				<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
+			</div>
+
 		</div>
 
-		<div class="item">
-			<span class="welcome__stars">
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-			</span>
-			<blockquote>
-				<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
-				<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
-			</blockquote>
-			<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
-		</div>
-
-		<div class="item">
-			<span class="welcome__stars">
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-				<svg class="icon icon-star">
-					<use xlink:href="#icon-star"></use>
-				</svg>
-			</span>
-			<blockquote>
-				<p>Not only does WP Migrate DB Pro fill a glaring hole in my WordPress development. The support staff go out of their way to make sure that their plugin works, even when you are the one that broke it. Not just an essential plugin, an excellent company to buy from.</p>
-				<cite>- Trevor Green<br><a href="#">[ Agreenweb - @trevogre ]</a></cite>
-			</blockquote>
-			<a href="/migrate.php" class="btn <?php echo $btn_class; ?>">Get Plugin</a>
-		</div>
+	</div>
 
 </section>
